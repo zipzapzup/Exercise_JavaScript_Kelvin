715318bc75979e2d2a3f2d2b4ede5b58679b45b8
zipzapzup <kelvin.jumino@gmail.com>
2022-02-11 00:20:01 +1100

    Nodemon

--- Projects/Node_Server/src/node.js
+++ Projects/Node_Server/src/node.js
@@ -3,13 +3,25 @@
 // npm init -y
 // npm install express
 // npm install @babel/core @babel/node
+// npm install body-parser
 // npm start
+// npm install nodemon (After restart it saves it)
 import express from 'express';
 import { people } from './people.js';
+import { promises as fs } from 'fs';
+import bodyParser from 'body-parser';
+// fs package is synchronous
+// Reading files is an async function
+// It uses promises
 
 let app = express();
+
+// After defining app, app.use is a plugin
+// BodyParser takes the extra argument from the request and places it in the request
+app.use(bodyParser.json());
+
 app.listen(8888,  () => {
-    console.log("Server is listening on port 8888")
+    console.log("Server is listening on port - 8888")
 })
 
 // Endpoint hello 
@@ -43,3 +55,27 @@ app.get('/people/:name', (request, response) => {
     i++;
     console.log("People Found:", name, " Count:", i)
 })
+
+// app.get('index', handler function)
+app.get('/file-data', async (request, response) => {
+
+    let data = await fs.readFile( new URL( 'people-data.json', import.meta.url));
+    // __dirname is the absolute path of node.js
+    // You need to call await to fs.readFile since it is an async function that returns a promise. Handle the callback in 3 different state: Pending, Accepted, Rejected.
+    let people = JSON.parse(data);
+    response.json(people);
+    // JSON Parse converts string to JSON Object
+
+
+});
+
+
+app.post('/people', (request, response) => {
+    let newPerson = request.body;
+    people.push(newPerson);
+    response.json(people);
+    i++;
+    console.log("POST Requests Received ", i)
+});
+
+// Node what to do add the JSON object and Write to FILE.
---
+++ Projects/Node_Server/src/test.js
@@ -0,0 +1,3 @@
+console.log("DIRNAME", import.meta.url)
+
+console.log("USING URL:", new URL('myfile.txt', import.meta.url))
