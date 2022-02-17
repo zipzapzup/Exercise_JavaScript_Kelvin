16970e0e4d4c0fcba0091b2ddb0e4e5825827a1a
zipzapzup <kelvin.jumino@gmail.com>
2022-02-17 20:41:30 +1100

    Append

--- Projects/Node_Scripts/fs.js
+++ Projects/Node_Scripts/fs.js
@@ -4,9 +4,15 @@ import path from 'path';
 const FilePath = new URL('hello.txt', import.meta.url);
 
 // read a file
-const data = fs.readFileSync( FilePath, 'utf-8', )
+const data = fs.readFileSync( FilePath, 'utf-8' )
 
 console.log(data)
 
 
 fs.writeFileSync(FilePath, 'New String and Words in Here', 'utf-8');
+
+fs.appendFileSync(FilePath, '\nAnother Line');
+
+const data2 = fs.readFileSync( FilePath, 'utf-8');
+
+console.log(data2);
