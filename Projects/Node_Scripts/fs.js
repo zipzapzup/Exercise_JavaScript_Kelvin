043531a8d9caba900afdbfcf66ae0b2bda811985
zipzapzup <kelvin.jumino@gmail.com>
2022-02-17 20:14:25 +1100

    File Manipulation

---
+++ Projects/Node_Scripts/fs.js
@@ -0,0 +1,12 @@
+import fs from "fs";
+import path from 'path';
+
+const FilePath = new URL('hello.txt', import.meta.url);
+
+// read a file
+const data = fs.readFileSync( FilePath, 'utf-8', )
+
+console.log(data)
+
+
+fs.writeFileSync(FilePath, 'New String and Words in Here', 'utf-8');
