4a22a1afd5f6fc03d7006de1d2db9a44be21c760
zipzapzup <kelvin.jumino@gmail.com>
2022-02-17 20:58:35 +1100

    FS

--- Projects/Node_Scripts/fs.js
+++ Projects/Node_Scripts/fs.js
@@ -12,7 +12,6 @@ console.log(data)
 fs.writeFileSync(FilePath, 'New String and Words in Here', 'utf-8');
 
 fs.appendFileSync(FilePath, '\nAnother Line');
-
 const data2 = fs.readFileSync( FilePath, 'utf-8');
 
 console.log(data2);
